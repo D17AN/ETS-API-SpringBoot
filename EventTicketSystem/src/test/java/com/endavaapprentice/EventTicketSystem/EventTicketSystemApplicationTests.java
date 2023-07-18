b02e6a5dea41c949a849c8a9e6242743fb6df9c4
D17AN <danybejenaru@yahoo.com>
2023-07-18 11:36:25 +0300

    Initialize new project

---
+++ EventTicketSystem/src/test/java/com/endavaapprentice/EventTicketSystem/EventTicketSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.endavaapprentice.EventTicketSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventTicketSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
