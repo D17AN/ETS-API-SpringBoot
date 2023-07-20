7b7806a468b05fb216e287a617976f01dd8b9e21
D17AN <danybejenaru@yahoo.com>
2023-07-20 14:53:20 +0300

    Create user service.

---
+++ EventTicketSystem/src/main/java/com/endavaapprentice/EventTicketSystem/service/UserService/UserService.java
@@ -0,0 +1,25 @@
+package com.endavaapprentice.EventTicketSystem.service.UserService;
+
+import com.endavaapprentice.EventTicketSystem.domain.Entity.User;
+import com.endavaapprentice.EventTicketSystem.repository.UserRepository.UserRepositoryInterface;
+
+import java.util.Collection;
+
+public class UserService implements UserServiceInterface{
+
+        private UserRepositoryInterface userRepository;
+
+        public UserService(UserRepositoryInterface userRepository){
+            this.userRepository = userRepository;
+        }
+
+        @Override
+        public Collection<User> getAllUser(){
+            return this.userRepository.findAll();
+        }
+
+        @Override
+        public User getUserById(Long userId){
+            return this.userRepository.findById(userId).get();
+        }
+}
