fd397dae749a284cc4836e6fef7861e4c313f4bb
D17AN <danybejenaru@yahoo.com>
2023-07-20 18:00:25 +0300

    Create location DTO.

---
+++ EventTicketSystem/src/main/java/com/endavaapprentice/EventTicketSystem/domain/DTO/EventVenueEventTypeDTO.java
@@ -0,0 +1,23 @@
+package com.endavaapprentice.EventTicketSystem.domain.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class EventVenueEventTypeDTO {
+    private Long eventID;
+    private VenueDTO venue;
+    private String eventType;
+    private String eventDescription;
+    private String eventName;
+    private String eventImageURL;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+    private List<TicketCategoryDTO> ticketCategoryDTOList;
+}
---
+++ EventTicketSystem/src/main/java/com/endavaapprentice/EventTicketSystem/domain/DTO/LocationDTO.java
@@ -0,0 +1,14 @@
+package com.endavaapprentice.EventTicketSystem.domain.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class LocationDTO {
+    private String countryName;
+    private String cityName;
+    private String address;
+}
