5cb464d692ac033d0e12a57c25b90061deef7512
D17AN <danybejenaru@yahoo.com>
2023-07-21 22:26:31 +0300

    Remove lombok annotations for models. Add getters, setters and constructors.

--- EventTicketSystem/src/main/java/com/endavaapprentice/EventTicketSystem/domain/Entity/EventType.java
+++ EventTicketSystem/src/main/java/com/endavaapprentice/EventTicketSystem/domain/Entity/EventType.java
@@ -2,19 +2,11 @@ package com.endavaapprentice.EventTicketSystem.domain.Entity;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.UniqueElements;
 
 import java.util.List;
 
 @Entity
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
 @Table(name = "[EventType]")
 public class EventType {
 
@@ -30,4 +22,43 @@ public class EventType {
     @OneToMany(mappedBy = "eventType", cascade = CascadeType.ALL,
             fetch = FetchType.LAZY)
     private List<Event> eventsList;
+
+    public EventType(Long eventTypeID, String eventTypeName, List<Event> eventsList) {
+        this.eventTypeID = eventTypeID;
+        this.eventTypeName = eventTypeName;
+        this.eventsList = eventsList;
+    }
+
+    public Long getEventTypeID() {
+        return eventTypeID;
+    }
+
+    public void setEventTypeID(Long eventTypeID) {
+        this.eventTypeID = eventTypeID;
+    }
+
+    public String getEventTypeName() {
+        return eventTypeName;
+    }
+
+    public void setEventTypeName(String eventTypeName) {
+        this.eventTypeName = eventTypeName;
+    }
+
+    public List<Event> getEventsList() {
+        return eventsList;
+    }
+
+    public void setEventsList(List<Event> eventsList) {
+        this.eventsList = eventsList;
+    }
+
+    @Override
+    public String toString() {
+        return "EventType{" +
+                "eventTypeID=" + eventTypeID +
+                ", eventTypeName='" + eventTypeName + '\'' +
+                ", eventsList=" + eventsList +
+                '}';
+    }
 }
